510b9830daf7c8a4e885b681d38a6da50ff5930d
Aaron Cordova <acordova05@gmail.com>
2017-12-30 06:42:46 -0700

    Adding privacy copy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -4,24 +4,227 @@ import { Box } from '../components/Layout';
 import colors from '../utils/colors';
 
 const Privacy = () => (
-  <Box bg={colors.primary}>
+  <Box bg={colors.white}>
     <Box
       width={[1, 1, 1 / 2]}
       m={['3.5rem 0 0 0', '3.5rem 0 0 0', '3.5rem auto 0 auto']}
       px={[3, 3, 0]}
-      color={colors.secondary}
+      color={colors.accent}
     >
       <h1>Privacy Policy</h1>
       <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        This Privacy Policy explains how information about you is collected,
+        used and disclosed by Meal Prep Planner and Diary (“MealPrep”) when you
+        use the mobile application (the “App”).
+      </p>
+      <p>
+        By accepting our Privacy Policy, you agree to let MealPrep collect, use
+        and disclose information about you in accordance with the purposes
+        listed below. The App is not intended for use by children under 13 and
+        we do not knowingly collect personal information from children under 13.
+        We may change this Privacy Policy from time to time. If we make changes,
+        we will notify you by revising the date at the top of the policy and,
+        depending on the specific changes, we may provide you with additional
+        notice or require a renewed consent (e.g., by adding a statement to our
+        website’s homepage or in the App). We encourage you to review the
+        Privacy Policy whenever you access the App to stay informed about our
+        information practices and the ways you can help protect your privacy.
+      </p>
+      <h3>Collection of Information</h3>
+      <h4>Information You Provide to Us</h4>
+      <p>
+        The App does not allow you to submit any information directly to us. If
+        you submit any information to MealPrep through our website or other
+        sites or services, please refer to the MealPrep Privacy Policy for
+        information about how we collect, use and disclose such information.
+      </p>
+      <h4>Information We Collect Automatically through the App</h4>
+      <p>
+        When you access or use the App, we automatically collect information
+        about you, including:
+      </p>
+      <ul>
+        <li>
+          Log Information: We log information about your use of the App,
+          including the type of device you use, the features you use, access
+          times, and your IP address.
+        </li>
+        <li>
+          Device Information: We collect information about the device you use to
+          access the App, including information about the device’s software and
+          hardware, unique device identifiers, push or other device tokens,
+          mobile network information and time zone.
+        </li>
+        <li>
+          Usage Information: We collect information relating to your use of the
+          App at an aggregated level only.
+        </li>
+        <li>
+          Consumption information: We collect information about your consumption
+          habits relating to your use of the App, including which purchases you
+          make with both virtual and real currencies.
+        </li>
+      </ul>
+      <h4>Information We Collect from Other Sources</h4>
+      <p>
+        We may also obtain information from other sources and combine that with
+        information we collect through the App. For example, if you log into the
+        App using a third-party site or platform such as Facebook, we may access
+        information about you from that site or platform, such as your screen
+        name, profile information and friend lists, in accordance with the
+        authorization procedures determined by such third party site or
+        platform. We may also collect information from app stores when you
+        download or update one of our applications.
+      </p>
+      <p>
+        If you log into the App using a third-party site or platform, you
+        represent and warrant that (i) your access and use of such features in
+        connection with the App will comply with the applicable terms and
+        policies of such site or platform; and (ii) that you are at least 13
+        years of age.
+      </p>
+      <h3>Use of Information</h3>
+      <p>
+        We may use information about you for various purposes, including to:
+      </p>
+      <ul>
+        <li>Provide, maintain and improve the App;</li>
+        <li>Develop new apps, sites and services;</li>
+        <li>
+          Provide and deliver the products and services you request and send you
+          related
+        </li>
+        information;
+        <li>
+          Send you technical notices, updates, security alerts and support and
+          administrative
+        </li>
+        messages;
+        <li>
+          Provide news and information about the App we think will be of
+          interest to you;
+        </li>
+        <li>
+          Personalize and improve the App and provide tailored content and
+          features;
+        </li>
+        <li>
+          Monitor and analyze trends, usage and activities in connection with
+          the App;
+        </li>
+        <li>
+          Link or combine with information we get from others to help understand
+          your needs
+        </li>
+        and provide you with better service; and
+        <li>
+          Carry out any other purpose for which the information was collected.
+        </li>
+      </ul>
+      <p>
+        By accepting our Privacy Policy or otherwise providing information to
+        us, you consent to the processing and transfer of information in and to
+        the U.S. and other countries.
+      </p>
+      <h3>Sharing of Information</h3>
+      <p>
+        We may share information about you as follows or as otherwise described
+        in this Privacy Policy:
+      </p>
+      <ul>
+        <li>
+          With vendors, consultants and other service providers who need access
+          to such information to carry out work on our behalf;
+        </li>
+        <li>
+          In response to a request for information if we believe disclosure is
+          in accordance with any applicable law, regulation or legal process, or
+          as otherwise required by any applicable law, rule or regulation;
+        </li>
+        <li>
+          If we believe your actions are inconsistent with the spirit or
+          language of our user agreements or policies, or to protect the rights,
+          property and safety of MealPrep or others;
+        </li>
+        <li>
+          In connection with, or during negotiations of, any merger, sale of
+          company assets, financing or acquisition of all or a portion of our
+          business to another company; and
+        </li>
+        <li>With your consent or at your direction.</li>
+      </ul>
+      <p>
+        If you log into the App using a third-party service, such as Facebook or
+        Apple Game Center, we may share information provided by those services
+        with other players for certain in-app features, such as leaderboards.
+      </p>
+      <p>
+        We also may share aggregated information or de-identified information,
+        which cannot reasonably be used to identify you.
+      </p>
+      <h4>Social Sharing Features</h4>
+      <p>
+        The App may offer social sharing features and other integrated tools
+        (such as the Facebook “Like” button), which let you share actions you
+        take in the App with other media, and vice versa. You must be at least
+        13 years of age to use any social sharing features integrated in the
+        App. The use of such features enables the sharing of information with
+        your friends or the public, depending on the settings you establish with
+        the entity that provides the social sharing feature. For more
+        information about the purpose and scope of data collection and
+        processing in connection with social sharing features, please visit the
+        privacy policies of the entities that provide these features.
+      </p>
+      <h3>Advertising and Analytics Services Provided by Others</h3>
+      <p>
+        We may allow third parties to serve contextual advertisements and
+        provide analytics services in connection with the App. These entities
+        may use various identifiers to collect information about your use of the
+        App, including your IP address, MAC address, device identifiers,
+        software and hardware information, time zone and usage information. This
+        information may be used by MealPrep and others to, among other things,
+        determine the popularity of certain content, deliver contextual
+        advertising and content and better understand your activity on the App.
+      </p>
+      <p>
+        Third-party ad serving and analytics technologies are integrated into
+        the App, so if you do not want to be subject to these technologies, do
+        not use or access the App.
+      </p>
+      Security
+      <p>
+        MealPrep takes reasonable measures to help protect information about you
+        from loss, theft, misuse and unauthorized access, disclosure, alteration
+        and destruction.
+      </p>
+      <h3>Your Choices</h3>
+      <h4>Account Information</h4>
+      <p>
+        You may request that we update, correct or delete information about you
+        at any time by emailing us at{' '}
+        <a href="mailto:support@leaniterations.com">
+          support@leaniterations.com
+        </a>, but note that we may retain certain information as required by law
+        or for legitimate business purposes. We may also retain cached or
+        archived copies of information about you for a certain period.
+      </p>
+      <h4>Push Notifications</h4>
+      <p>
+        With your consent, we may send push notifications or alerts to your
+        mobile device to provide game-related information, service updates,
+        promotional communications and other related messages. You can
+        deactivate these notifications by changing your notification settings on
+        your device.
+      </p>
+      <h4>Storage of Information</h4>
+      <p>
+        MealPrep will store your information for as long as necessary in order
+        to provide you with the App.
+      </p>
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions about this Privacy Policy, please{' '}
+        <a href="mailto:support@leaniterations.com">contact us</a>.
       </p>
     </Box>
   </Box>
