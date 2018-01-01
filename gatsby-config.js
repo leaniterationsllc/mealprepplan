571e38391124a0b6f814706d5bd54dd8a0c20a2a
Aaron Cordova <acordova05@gmail.com>
2018-01-01 07:25:07 -0700

    Making some minor tweaks
    - Adding a favicon
    - Adding google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,25 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: ``
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING
+      }
+    },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
       }
     },
     'gatsby-transformer-remark',
